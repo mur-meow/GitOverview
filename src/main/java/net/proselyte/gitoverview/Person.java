39b1eb12cfd5a14f31922871881b72e65c298759
Egor Leontiew <egor.leontiew2016@yandex.ru>
2020-10-31 19:38:54 +0300

    Adding class Person

---
+++ src/main/java/net/proselyte/gitoverview/Person.java
@@ -0,0 +1,27 @@
+package net.proselyte.gitoverview;
+
+public class Person extends BaseEntity {
+
+    private String firstname;
+
+    private String lastName;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+
